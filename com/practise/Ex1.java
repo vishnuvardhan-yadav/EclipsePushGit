772f6849a4edadcceb3e48d1b79733adc9379807
Vishnuvardhan yadav <vardhanvishnu60027@gmail.com>
2026-07-15 11:22:32 +0530

    Second time

--- com/practise/Ex1.java
+++ com/practise/Ex1.java
@@ -3,33 +3,36 @@ package com.practise;
 import java.util.Scanner;
 
 public class Ex1 {
-    public static void  primeNum(int num) {
-        if(num<=1) {
-        	System.out.println("Not a prime");
-        	return;
-        }
-        int i;
-        for(i=2; i*i<num; i++) {
-        		if(num%i == 0) {
-        			break;
-        			
-        		}        		
-        		
-        }
-        if(i*i>num) {
-			System.out.println("Is Prime");
-		} else {
-			System.out.println("Not a Prime");
-		}
-        
-    }
+    //public static void  primeNum(int num) {
+//        if(num<=1) {
+//        	System.out.println("Not a prime");
+//        	return;
+//        }
+//        int i;
+//        for(i=2; i*i<num; i++) {
+//        		if(num%i == 0) {
+//        			break;
+//        			
+//        		}        		
+//        		
+//        }
+//        if(i*i>num) {
+//			System.out.println("Is Prime");
+//		} else {
+//			System.out.println("Not a Prime");
+//		}
+//        
+//    }
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the Num:");
-
-        int num = input.nextInt();
-        primeNum(num);
-
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the Num:");
+//
+//        int num = input.nextInt();
+//        primeNum(num);
+    	
+    		final int num = 5;
+    		int num = 10;
+    		System.out.println(num);
     }
 }
