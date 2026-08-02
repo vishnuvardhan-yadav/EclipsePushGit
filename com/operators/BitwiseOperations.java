75bac1f1be61091d0f2af0b0cb098c5598b6d963
Vishnuvardhan yadav <vardhanvishnu60027@gmail.com>
2026-08-02 11:50:13 +0530

    Refactor BitwiseOperations to LeftShiftOperator
    Refactored BitwiseOperations class to LeftShiftOperator and simplified the main method to demonstrate left shift operation.

--- com/operators/BitwiseOperations.java
+++ com/operators/BitwiseOperations.java
@@ -1,22 +1,14 @@
 package com.operators;
 
-import java.util.Scanner;
-
-public class BitwiseOperations {
+public class LeftShiftOperator {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int num = 10;
+        int shift = 2;
 
-        System.out.print("Enter First Number : ");
-        int num1 = sc.nextInt();
+        System.out.println("Enter Number          : " + num);
+        System.out.println("Enter Shift Positions : " + shift);
 
-        System.out.print("Enter Second Number : ");
-        int num2 = sc.nextInt();
-
-        System.out.println("Bitwise AND : " + (num1 & num2));
-        System.out.println("Bitwise OR : " + (num1 | num2));
-        System.out.println("Bitwise XOR : " + (num1 ^ num2));
-
-        sc.close();
+        System.out.println("Result After Left Shift : " + (num << shift));
     }
 }
