1ad269e101b9c5d10fb50bd5100cf8ca427c0ac3
Vishnuvardhan yadav <vardhanvishnu60027@gmail.com>
2026-08-02 11:41:27 +0530

    Fix output message for right shift operation

--- com/operators/RightShiftOperator.java
+++ com/operators/RightShiftOperator.java
@@ -1,19 +1,19 @@
 package com.operators;
+
 import java.util.Scanner;
 
 public class RightShiftOperator {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Number: ");
+        System.out.print("Enter Number : ");
         int num = sc.nextInt();
 
         System.out.print("Enter Shift Positions : ");
         int shift = sc.nextInt();
 
-        int result = num >> shift;
-
-        System.out.println("Result After Right Shift : " + result);
+        System.out.println("Result After Right Shift : " + (num >> shift));
 
         sc.close();
     }
