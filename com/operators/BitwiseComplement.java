32f771934bf06ccfe4b4c17ff78e106348ddf613
Vishnuvardhan yadav <vardhanvishnu60027@gmail.com>
2026-08-02 11:48:38 +0530

    Update BitwiseComplement.java

--- com/operators/BitwiseComplement.java
+++ com/operators/BitwiseComplement.java
@@ -1,22 +1,16 @@
 package com.operators;
 
-import java.util.Scanner;
-
 public class BitwiseOperations {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int num1 = 12;
+        int num2 = 10;
 
-        System.out.print("Enter First Number : ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter Second Number : ");
-        int num2 = sc.nextInt();
+        System.out.println("Enter First Number  : " + num1);
+        System.out.println("Enter Second Number : " + num2);
 
         System.out.println("Bitwise AND : " + (num1 & num2));
-        System.out.println("Bitwise OR : " + (num1 | num2));
+        System.out.println("Bitwise OR  : " + (num1 | num2));
         System.out.println("Bitwise XOR : " + (num1 ^ num2));
-
-        sc.close();
     }
 }
